4fb6b2580f8af826e7757f841cebcd3793cbd4b1
Hendrik Rombach <hr@netinventors.de>
2017-04-22 22:03:09 +0200

    Fix diagonal movement speed

---
+++ Assets/scripts/movement/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speedFactor = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var vector3 = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.Translate(vector3.normalized * Time.deltaTime * speedFactor);
+    }
+}
