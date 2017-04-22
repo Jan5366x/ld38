777820b1b56b3e18e3f4f7005b1cd86d5830d7e5
Jan Schwien <jan5366@gmail.com>
2017-04-22 16:12:12 +0200

    started game controller work, adjusted texture settings

---
+++ Assets/scripts/world/WorldController.cs
@@ -0,0 +1,34 @@
+﻿using Assets.scripts.world;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+    public Transform[] worldElements;
+    public int sectorSize = 64;
+
+    private WorldSector[,] sectors = new WorldSector[9,9];
+
+    // Use this for initialization
+    void Start () {
+        testFill();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    // test method
+    private void testFill() {
+
+  
+        for (int x = 0; x < sectors.GetUpperBound(0); x++) {
+            for (int y = 0; y < sectors.GetUpperBound(1); y++)
+            {
+                Instantiate(worldElements[Random.Range(0, worldElements.GetUpperBound(0))], new Vector3(x + 0.5f, -y + 0.5f, 0), new Quaternion());
+            }
+        }
+    }
+}
