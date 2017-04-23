e51771beef76ffe52a1282b8b402a9435a05aa61
Hendrik Rombach <hr@netinventors.de>
2017-04-23 18:27:15 +0200

    Add proximity enemy spawn to infector

---
+++ Assets/scripts/world/action/SpawnEnemyAction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace world.action
+{
+    public class SpawnEnemyAction : MonoBehaviour
+    {
+        public float SpawnDelay = 5f;
+
+        private GameObject _player;
+
+        public void Start()
+        {
+            _player = GameObject.Find("Player");
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.Equals(_player)) StartCoroutine("SpawnEnemies");
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.Equals(_player)) StopCoroutine("SpawnEnemies");
+        }
+
+        public IEnumerator SpawnEnemies()
+        {
+            for (;;)
+            {
+                Debug.Log("SPAWN!");
+                yield return new WaitForSeconds(SpawnDelay);
+            }
+        }
+    }
+}
