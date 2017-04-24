4498f1aac67fc3f0d21625ae44735c9461ea6059
Jan Schwien <jan5366@gmail.com>
2017-04-24 19:54:43 +0200

    work on level building

---
+++ Assets/AutoGroundName.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class AutoGroundName : MonoBehaviour {
+
+    private Vector3 lastLocation;
+
+	// Use this for initialization
+	void Awake () {
+        autoName();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        autoName();
+    }
+
+    private void autoName() {
+        if (!transform.position.Equals(lastLocation)) {
+            name = "G-" + (int)transform.position.x + "-" + (int)-transform.position.y;
+
+            lastLocation = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/RandomOrder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomOrder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sortingOrder = (int)Random.Range(1f, 1000.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
