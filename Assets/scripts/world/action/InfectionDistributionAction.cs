6a700c6d60ef04396608eb23e5d6b2851c76d7a3
Jan Schwien <jan5366@gmail.com>
2017-04-25 01:10:07 +0200

    added tree(s)

--- Assets/scripts/world/action/InfectionDistributionAction.cs
+++ Assets/scripts/world/action/InfectionDistributionAction.cs
@@ -6,19 +6,7 @@ using System;
 
 public class InfectionDistributionAction : MonoBehaviour, IWorldAction {
 
-
     public float infectionPower = 0.1f;
-
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void worldUpdate(WorldController controller, WorldSector sector, int locX, int locY)
     {
---
+++ Assets/scripts/world/action/InfectionSpriteReplaceAction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.scripts.world.action
+{
+    public class InfectionSpriteReplaceAction : MonoBehaviour, IWorldAction
+    {
+
+        public Sprite normalSprite;
+        public Sprite infectedSprite;
+
+        private bool currentState = false;
+
+        public void worldUpdate(WorldController controller, WorldSector sector, int locX, int locY)
+        {
+
+            if (sector == null)
+                return;
+
+            bool isInfected = sector.IsInfected;
+
+            if (currentState != isInfected) {
+
+                if (isInfected)
+                {
+                    if (infectedSprite != null)
+                    {
+                        GetComponent<SpriteRenderer>().sprite = infectedSprite;
+                    }
+                }
+                else
+                {
+                    if (normalSprite != null)
+                    {
+                        GetComponent<SpriteRenderer>().sprite = normalSprite;
+                    }
+                }
+
+
+                // update flag
+                currentState = isInfected;
+            }
+
+        }
+    }
+}
