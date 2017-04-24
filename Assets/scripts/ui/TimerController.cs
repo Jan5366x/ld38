65ffe602f69c926d22000b05f37051cee8d55c57
Hendrik Rombach <hr@netinventors.de>
2017-04-24 22:02:27 +0200

    Add game timer to MainUI

---
+++ Assets/scripts/ui/TimerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+namespace ui
+{
+    public class TimerController : MonoBehaviour
+    {
+        private static Text _timerText;
+        private float _startTime;
+
+
+        private void Start()
+        {
+            var timerObj = GameObject.Find("MainUI/Timer");
+            if (timerObj != null) _timerText = timerObj.GetComponent<Text>();
+
+            _startTime = Time.time;
+        }
+
+
+        private void FixedUpdate()
+        {
+            var elapsed = Time.time - _startTime;
+
+            var minutes = Mathf.RoundToInt(Mathf.Floor(elapsed / 60));
+            var seconds = Mathf.RoundToInt(elapsed%60);
+
+            if (_timerText != null)
+            {
+                    _timerText.text = string.Format("{0}:{1:00}", minutes, seconds);
+            }
+        }
+    }
+}
